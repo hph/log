6a29ef16f3295ec56482dbe10ac6eaaee85658e0
Haukur Páll Hallvarðsson <haukurpallh@gmail.com>
2013-10-26 00:14:22 +0000

    Simplify requirement

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'haml'
 require 'sinatra'
 require 'sinatra/activerecord'
 
-(Dir['./models/*.rb'] + Dir['./config/*.rb']).each { |file| require file }
+Dir['./models/*.rb', './config/*.rb'].each { |file| require file }
 
 get '/' do
   @statuses = Status.select(:status).order('created_at DESC')
